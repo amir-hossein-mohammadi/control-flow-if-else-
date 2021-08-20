63efc587aba66227c657a256f5925de7f931c888
amir-hossein-mohammadi <68467741+amir-hossein-mohammadi@users.noreply.github.com>
2021-08-20 17:51:59 +0430

    initial commit

---
+++ js/controlFlow.js
@@ -0,0 +1,17 @@
+let x = prompt("add a number: ");
+
+if (x >= 17 && x <= 20 ) {
+    alert("very well")
+} else if (x > 15 && x == 15) {
+    alert("good")
+}else if (x < 15 && x > 12) {
+    alert("not bad")
+}else if (x <= 12 && x >= 10) {
+    alert("on edge, please try more")
+}else if (x < 10 && x >= 0) {
+    alert("you are rejected")
+}else {
+    alert("null")
+};
+
+
